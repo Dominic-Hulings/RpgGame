4e80aa2446d77ee07de924f8bd0e04db7877eb04
dominic-hulings <dominichulings@gmail.com>
2024-12-13 09:45:32 -0500

    Forgot two files from last commit

---
+++ Rpg/Game/Item/BaseItem.cs
@@ -0,0 +1,11 @@
+namespace Rpg.Game.Item;
+
+public abstract class BaseItem
+{
+  // Fields
+  public string itemName = "";
+  public string itemDescription = "";
+  public bool isWearable;
+  public bool isWieldable;
+  public bool isConsumable;
+}
---
+++ Rpg/Game/NPC/BaseNpc.cs
@@ -0,0 +1,10 @@
+namespace Rpg.Game.NPC;
+
+public abstract class BaseNpc
+{
+  public string Name { get; set; } = "";
+  
+  public bool isFemale { get; set; }
+  
+  // TODO: Add more methods and properties/fields
+}
